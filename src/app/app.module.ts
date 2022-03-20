aca46f1342599e09776bde798f8f5a4bca7c335f
shenoudak <shenoudakaram32@gmail.com>
2022-03-21 01:31:35 +0200

    dashboard_Shenouda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/material/material.module';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/material/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
+import{MatButtonToggleModule} from '@angular/material/button-toggle';
+import{MatSidenavModule} from '@angular/material/sidenav';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{MatMenuModule} from '@angular/material/menu';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+
+const materialComponents=
+[
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatDividerModule,
+  MatListModule,
+  MatIconModule,
+  MatSelectModule
+
+
+]
+
+@NgModule({
+  imports: [
+    materialComponents
+  ],
+  exports:
+  [
+    materialComponents
+  ]
+})
+export class MaterialModule { }
